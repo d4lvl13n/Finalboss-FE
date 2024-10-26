708c04d797c25925890a7821b95acc805b845d73
Damien Larquey <d4lvl13n@protonmail.com>
2024-10-26 11:36:55 +0200

    cors update

--- app/lib/apolloClient.ts
+++ app/lib/apolloClient.ts
@@ -3,20 +3,15 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
 const httpLink = createHttpLink({
   uri: 'https://backend.finalboss.io/graphql',
-  credentials: 'same-origin',
+  credentials: 'include',
   headers: {
     'Content-Type': 'application/json',
   }
 });
 
 const client = new ApolloClient({
-  uri: 'https://backend.finalboss.io/graphql',
+  link: httpLink,
   cache: new InMemoryCache(),
-  credentials: 'include',
-  headers: {
-    'Content-Type': 'application/json',
-    // The nonce will be automatically handled by WordPress
-  },
 });
 
 export default client;
